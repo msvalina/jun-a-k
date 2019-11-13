62879809091419e6a0a0c2cfa7b979212fe85ca8
Marijan Svalina <marijan.svalina@gmail.com>
2019-11-13 11:33:53 +0100

    Add ReportModel

---
+++ yoonak/README.md
@@ -0,0 +1,11 @@
+Dependencies
+============
+
+OS package level:
+
+- libimage-exiftool-perl
+
+TODO
+****
+
+1. When there is many OS level dependencies make a docker image
--- yoonak/reports/models.py
+++ yoonak/reports/models.py
@@ -1,25 +1,29 @@
 from django.db import models
+
 from uuid import uuid4
 import datetime
+import os
 
 # Create your models here.
 
 
 def image_direcotry_path_uuid(instance, filename):
     year_month = datetime.date.today().strftime("%Y/%m")
-    return 'uploads/{0}/{1}'.format(year_month, uuid4())
+    ext = os.path.splitext(filename)[1]
+    return 'uploads/{0}/{1}{2}'.format(year_month, str(uuid4())[-12:], ext)
 
 class ReportModel(models.Model):
     """Model for reported images"""
+    # TODO convert to jpg and resize to file size less then XX
 
-    created_at = models.DateTimeField('created at')
-    img_created_at = models.DateTimeField('image created at')
-    # file will be saved to MEDIA_ROOT/uploads/2019/01/uuid
-    reported_img = models.ImageField(upload_to=image_direcotry_path_uuid)
+    created_at = models.DateTimeField('created at', auto_now=True)
+    created_at_img = models.DateTimeField('image created at')
+    # file will be saved to MEDIA_ROOT/uploads/2019/01/uuid.ext
+    image = models.ImageField(upload_to=image_direcotry_path_uuid)
+    # Precission https://stackoverflow.com/a/30711177 up to 10 cm
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
 
-
-    def __str__(self):
-        pass
 
     class Meta:
         db_table = ''
