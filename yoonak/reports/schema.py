772c0b39843c4c3bf14e313c15c3ac074281ef49
Marijan Svalina <marijan.svalina@gmail.com>
2019-11-14 12:48:17 +0100

    Add pylint disable E0213 no-self-arg

--- yoonak/reports/schema.py
+++ yoonak/reports/schema.py
@@ -7,9 +7,11 @@ class Query(ObjectType):
     reports = DjangoConnectionField(ReportType)
     report = Field(ReportType, id=Argument(ID, required=True))
 
+    # pylint: disable=E0213
     def resolve_reports(root, info, **kwargs):
         return Report.objects.all()
 
+    # pylint: disable=E0213
     def resolve_report(root, info, **kwargs):
         return Report.objects.get(id=kwargs.get('id'))
 
