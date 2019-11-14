ede9b926840324267dcfde6dca738d607da38897
Marijan Svalina <marijan.svalina@gmail.com>
2019-11-14 10:27:49 +0100

    Add description field

--- yoonak/reports/models.py
+++ yoonak/reports/models.py
@@ -16,10 +16,11 @@ class ReportModel(models.Model):
     """Model for reported images"""
     # TODO convert to jpg and resize to file size less then XX
 
-    created_at = models.DateTimeField('created at', auto_now=True)
-    created_at_img = models.DateTimeField('image created at')
+    created_at = models.DateTimeField('Created at', auto_now=True)
+    created_at_img = models.DateTimeField('Image created at')
     # file will be saved to MEDIA_ROOT/uploads/2019/01/uuid.ext
     image = models.ImageField(upload_to=image_direcotry_path_uuid)
+    description = models.CharField(max_length=500)
     # Precission https://stackoverflow.com/a/30711177 up to 10 cm
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
