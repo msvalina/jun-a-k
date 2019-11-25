5cf83ef7d219cee91d52a09973ccf7db8eee9bfb
Marijan Svalina <marijan.svalina@gmail.com>
2019-11-25 10:37:15 +0100

    Add CTA report button to Jumbotron

--- front/src/components/Jumbotron.js
+++ front/src/components/Jumbotron.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import {
+  Jumbotron as Jumbo,
+  Container,
+  Button,
+  Nav,
+ } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import heroImage from '../assets/images/boy-junak-big.jpeg';
 
@@ -8,9 +14,9 @@ const Styles = styled.div`
     background: url(${heroImage}) no-repeat fixed bottom;
     background-size: cover;
     color: #efefef;
-    height: 900px;
+    height: 1100px;
     position: relative;
-    z-index: -2;
+    z-index: 0;
   }
 
   .overlay {
@@ -23,7 +29,12 @@ const Styles = styled.div`
     right: 0;
     z-index: -1;
   }
+
+  .btn {
+    z-index: 1;
+  }
 `;
+
 
 export const Jumbotron = () => (
   <Styles>
@@ -34,6 +45,9 @@ export const Jumbotron = () => (
         <p>Save the planet!</p>
         <p>Reporting one wild dump at a time!</p>
         <p>Get Bitcoins for it!</p>
+        <Button size="lg" variant="dark" className="btn">
+        <Nav.Link as={Link} to="/report" className="btn-dark">Report</Nav.Link>
+        </Button>
       </Container>
     </Jumbo>
   </Styles>
