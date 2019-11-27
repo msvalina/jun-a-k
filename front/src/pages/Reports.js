87dec3f6abb7a21abe35800c088c428fe3bf3931
Marijan Svalina <marijan.svalina@gmail.com>
2019-11-27 10:25:35 +0100

    Add pagination to reports

--- front/src/pages/Reports.js
+++ front/src/pages/Reports.js
@@ -1,55 +1,92 @@
-import React, { Fragment } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Spinner from 'react-bootstrap/Spinner'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
 
-import { Layout } from '../components';
+import { Layout } from "../components";
 
 const GET_REPORTS = gql`
-    query reportsList($after: String) {
-        reports(after: $after){
-            edges {
-                node {
-                    id
-                    createdAt
-                    description
-                    location
-                }
-            }
+  query reportsList($after: String) {
+    reports(first: 7, after: $after) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          createdAt
+          description
+          location
         }
+      }
+    }
   }
 `;
 
 export default function Reports() {
-    const { data, loading, error } = useQuery(GET_REPORTS);
-    if (loading) return (
-        <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
-        </Spinner>
-    );
-    if (error) {
-        console.log(error);
-        return <p>ERROR </p>;
-    }
-
+  const { data, loading, error, fetchMore } = useQuery(GET_REPORTS);
+  if (loading)
     return (
-        <Layout>
-        <Fragment>
-            {data.reports &&
-             data.reports.edges &&
-             data.reports.edges.map(edge => (
-                 <Card style={{ width: '18rem' }} >
-                     <Card.Body>
-                        <Card.Title>{edge.node.location} {edge.node.id}</Card.Title>
-                        <Card.Text>{edge.node.description}</Card.Text>
-                        <Button variant="light">Do something</Button>
-                     </Card.Body>
-                </Card>
-             ))}
-        </Fragment>
-        </Layout>
-   )
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    );
+  if (error) {
+    console.log(error);
+    return <p>ERROR </p>;
+  }
+
+  return (
+    <Layout>
+      <Fragment>
+        {data.reports &&
+          data.reports.edges &&
+          data.reports.edges.map(edge => (
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>
+                  {edge.node.location} {edge.node.id}
+                </Card.Title>
+                <Card.Text>{edge.node.description}</Card.Text>
+                <Button variant="light">Do something</Button>
+              </Card.Body>
+            </Card>
+          ))}
+        {data.reports &&
+          data.reports.pageInfo &&
+          data.reports.pageInfo.hasNextPage && (
+            <Button
+              onClick={() =>
+                fetchMore({
+                  variables: {
+                    after: data.reports.pageInfo.endCursor
+                  },
+                  updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                    if (!fetchMoreResult) return prev;
+                    return {
+                      ...fetchMoreResult,
+                      reports: {
+                        ...fetchMoreResult.reports,
+                        edges: [
+                          ...prev.reports.edges,
+                          ...fetchMoreResult.reports.edges
+                        ]
+                      }
+                    };
+                  }
+                })
+              }
+            >
+              Load More
+            </Button>
+          )}
+      </Fragment>
+    </Layout>
+  );
 }
